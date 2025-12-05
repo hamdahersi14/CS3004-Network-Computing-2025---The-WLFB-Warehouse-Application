ab8e3be32da84082cd4d61ccf395a7413359e4f4
hamdahersi14 <hamda.hersi14@gmail.com>
2025-12-05 04:54:22 +0000

    Add files via upload
    Main server class for the WLFB Warehouse Application.

---
+++ WLFBServer.java
@@ -0,0 +1,42 @@
+import java.net.*;
+import java.io.*;
+
+
+
+
+public class WLFBServer {
+  public static void main(String[] args) throws IOException {
+
+	ServerSocket ActionServerSocket = null;
+    boolean listening = true;
+    String ActionServerName = "WLFBServer";
+    int ActionServerNumber = 5000;
+    
+     int startApples = 1000;
+     int startOranges = 1000;
+
+    //Create the shared object in the global scope...
+    
+    SharedActionState ourSharedActionStateObject = new SharedActionState( startApples, startOranges);
+        
+    // Make the server socket
+
+    try {
+      ActionServerSocket = new ServerSocket(ActionServerNumber);
+    } catch (IOException e) {
+      System.err.println("Could not start " + ActionServerName + " specified port.");
+      System.exit(-1);
+    }
+    System.out.println(ActionServerName + " started");
+
+    //Got to do this in the correct order with only four clients!  Can automate this...
+    
+    while (listening){
+      new WLFBServerThread(ActionServerSocket.accept(),"WLFBThread1",ourSharedActionStateObject).start();
+      new WLFBServerThread(ActionServerSocket.accept(),"WLFBThread2", ourSharedActionStateObject).start();
+      new WLFBServerThread(ActionServerSocket.accept(),"WLFBThread3", ourSharedActionStateObject).start();
+      System.out.println("New " + ActionServerName + " thread started.");
+    }
+    ActionServerSocket.close();
+  }
+}
